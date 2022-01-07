bdcdad62c871ba82d5af06448e82aae5518b1427
ballhog23 <67295379+ballhog23@users.noreply.github.com>
2022-01-07 12:45:12 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+"use-strict";
+
+const onClick = () => {
+    let joke = document.getElementById('joke');
+    callUrl('https://api.chucknorris.io/jokes/random')
+        .then((v) => {
+            joke.innerText = v.value;
+        })
+}
+
+async function callUrl(url) {
+    try {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        return data;
+    } catch (err) {
+        console.log('something is wrong', err)
+    }
+}
